c14aaed241913913fc3ce613078d53aba04f981f
Ravi Mahajan <40781292+ravidmahajan@users.noreply.github.com>
2023-02-14 23:27:44 -0500

    Time: 1028 ms (30.83%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+
+SELECT w1.id 
+FROM Weather w1, Weather w2
+WHERE w1.Temperature > w2.Temperature AND 
+      TO_DAYS(w1.recordDate)-TO_DAYS(w2.recordDate)=1;
