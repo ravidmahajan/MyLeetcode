1a7337dba9ca3d925dff926a2be63201d1d76aff
Ravi Mahajan <40781292+ravidmahajan@users.noreply.github.com>
2023-04-04 07:30:14 -0400

    Time: 7 ms (78.08%), Space: 45.9 MB (48.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -8,9 +8,11 @@ class Solution {
             char[] charArray = str.toCharArray();
             Arrays.sort(charArray);
             String sorted = String.valueOf(charArray);
+            //if key is not present in HashMap
             if(!map.containsKey(sorted)){
                 map.put(sorted, new ArrayList<>());
             }
+            //if key is present in HashMap
             map.get(sorted).add(str);
         }
         return new ArrayList<>(map.values());
