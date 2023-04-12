0bbe23038e65343ca46b58c099c491e1b3a7a4ab
Ravi Mahajan <40781292+ravidmahajan@users.noreply.github.com>
2023-04-12 13:55:03 -0400

    Time: 5 ms (92.59%), Space: 42 MB (90.81%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -2,27 +2,32 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
      if(s.length() != t.length()) return false;
         
-        // 1 hashMap and 1 hasSet Solution
+        // 2Char Array
         
-        HashMap<Character, Character> sMap = new HashMap<>();
-        HashSet<Character> tSet = new HashSet<>();
+        char[] sArray = new char[100];
+        char[] tArray = new char[100];
         
         for(int i = 0; i < t.length() ; i++){
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
             
-            if(sMap.containsKey(sChar)){
-                if(sMap.get(sChar) != tChar){
-                    return false;
-                }
-            } else {
-                
-                if(tSet.contains(tChar)){
-                    return false;
-                }
-                sMap.put(sChar, tChar);
-                tSet.add(tChar);
-            }
+            //sArray
+         if(sArray[sChar - ' '] != 0){
+             if(sArray[sChar - ' '] != tChar){
+                 return false;
+             }
+         } else {
+             sArray[sChar - ' '] = tChar;
+         }
+            //tArray
+         if(tArray[tChar - ' '] != 0){
+             if(tArray[tChar - ' '] != sChar){
+                 return false;
+             }
+         } else {
+             tArray[tChar - ' '] = sChar;
+         }
+            
         }
         return true;
     }
