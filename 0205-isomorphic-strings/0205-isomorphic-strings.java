297c4040f868adc9827966a99351ca38cbe12c4b
Ravi Mahajan <40781292+ravidmahajan@users.noreply.github.com>
2023-04-12 13:55:44 -0400

    Time: 10 ms (61.72%), Space: 42.7 MB (25.03%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -2,32 +2,27 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
      if(s.length() != t.length()) return false;
         
-        // 2Char Array
+        // 1 hashMap and 1 hasSet Solution
         
-        char[] sArray = new char[100];
-        char[] tArray = new char[100];
+        HashMap<Character, Character> sMap = new HashMap<>();
+        HashSet<Character> tSet = new HashSet<>();
         
         for(int i = 0; i < t.length() ; i++){
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
             
-            //sArray
-         if(sArray[sChar - ' '] != 0){
-             if(sArray[sChar - ' '] != tChar){
-                 return false;
-             }
-         } else {
-             sArray[sChar - ' '] = tChar;
-         }
-            //tArray
-         if(tArray[tChar - ' '] != 0){
-             if(tArray[tChar - ' '] != sChar){
-                 return false;
-             }
-         } else {
-             tArray[tChar - ' '] = sChar;
-         }
-            
+            if(sMap.containsKey(sChar)){
+                if(sMap.get(sChar) != tChar){
+                    return false;
+                }
+            } else {
+                
+                if(tSet.contains(tChar)){
+                    return false;
+                }
+                sMap.put(sChar, tChar);
+                tSet.add(tChar);
+            }
         }
         return true;
     }
