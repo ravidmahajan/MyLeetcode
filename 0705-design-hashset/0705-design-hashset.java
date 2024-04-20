5a8493b9061b5e1b71455127b0c2540e4b8c3fae
Ravi Mahajan <40781292+ravidmahajan@users.noreply.github.com>
2024-04-20 08:22:10 +0530

    Time: 12 ms (98.52%), Space: 49.2 MB (84.38%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,54 +1,55 @@
 class MyHashSet {
-    boolean[][] storage; //array
+    boolean[][] storage;
     int buckets, bucketItems;
+    
     
     public MyHashSet() {
         buckets = 1000;
         bucketItems = 1000;
-        storage = new boolean[buckets][]; // initialize array
+        storage = new boolean[buckets][];
     }
     
-    //first hash function
-    int getBucket(int key){
-        return key % buckets;
+    int getBuckets(int key){
+        return key%buckets;
     }
     
-    //second hash function
     int getBucketItems(int key){
-        return key / bucketItems;
+        return key/bucketItems;
     }
     
     public void add(int key) {
-        int bucket = getBucket(key);
+        int bucket = getBuckets(key);
         int bucketItem = getBucketItems(key);
         
         if(storage[bucket] == null){
             if(bucket == 0){
-               storage[bucket] = new boolean[bucketItems + 1]; 
+                storage[bucket] = new boolean[bucketItems + 1];
             } else {
                 storage[bucket] = new boolean[bucketItems];
             }
         }
+        
         storage[bucket][bucketItem] = true;
     }
     
     public void remove(int key) {
-        int bucket = getBucket(key);
+        int bucket = getBuckets(key);
         int bucketItem = getBucketItems(key);
+        
         if(storage[bucket] == null){
             return;
         }
-         storage[bucket][bucketItem] = false;
+        storage[bucket][bucketItem] = false;
     }
-        
+    
     public boolean contains(int key) {
-        int bucket = getBucket(key);
+        int bucket = getBuckets(key);
         int bucketItem = getBucketItems(key);
+        
         if(storage[bucket] == null){
             return false;
         }
-            return storage[bucket][bucketItem];   
-        
+        return storage[bucket][bucketItem];
     }
 }
 
